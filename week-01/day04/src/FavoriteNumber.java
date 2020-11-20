9cb64b0a1717dde49cc1afe3e1105d9eee6e69f3
Semse Izsák <semseizsak@gmail.com>
2020-11-20 10:43:19 +0100

    Just added all exercises from day4 basic java

--- week-01/day04/src/FavoriteNumber.java
+++
@@ -1,9 +0,0 @@
-public class FavoriteNumber {
-    public static void main(String[] args){
-        // Store your favorite number in a variable (as a number)
-        // And print it like this: "My favorite number is: 8"
-        int favoriteNumber = 8008135;
-        System.out.println("My favourite number is: " + favoriteNumber);
-
-    }
-}
--- week-01/day04/src/IntroduceYourself.java
+++
@@ -1,7 +0,0 @@
-public class IntroduceYourself {
-    public static void main(String[] args) {
-        System.out.println("Semsei Izsák");
-        System.out.printf("20");
-        System.out.printf("1.74");
-    }
-}
