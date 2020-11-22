2496ca0e2462b275f29d58a2b47aa6a24375e073
Semse Izsák <semseizsak@gmail.com>
2020-11-22 09:12:40 +0100

    19.guessTheNumber

---
+++ week-01/day04/Tasks/guessTheNumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+// Write a program that stores a number,
+// and the user has to figure it out.
+// The user can input guesses,
+// after each guess the program would tell one
+// of the following:
+//
+// The stored number is higher
+// The stored number is lower
+// You found the number: 8
+public class guessTheNumber {
+
+}
